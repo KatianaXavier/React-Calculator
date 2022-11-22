16c1b349a9cf42ba9e69986add962709aaf75c76
katianaxavierb@gmail.com <katianaxavierb@gmail.com>
2022-11-22 19:23:16 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #D1D1D1;
+    background-color: #51A3A3;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+    
+    &:hover {
+        opacity: 0.6;
+        transition: 0.3s;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #DD7373;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Open Sans';
+    
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #DD7373;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align-last: center;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Open Sans';
+        color: #FFFFFF;
+    }
+`
